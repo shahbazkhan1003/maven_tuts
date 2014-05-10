4ff213163c551177921e13c53efa8d65ab5faf6c
jocnud@gmail.com <jocnud@gmail.com>
2014-05-10 19:56:40 +0530

    Second Commit

--- maventuts/src/main/java/com/shahbaz/blog/maventuts/App.java
+++ maventuts/src/main/java/com/shahbaz/blog/maventuts/App.java
@@ -8,5 +8,6 @@ package com.shahbaz.blog.maventuts;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(" Hello world in maven");
+		System.out.println(" Again Hello");
 	}
 }
