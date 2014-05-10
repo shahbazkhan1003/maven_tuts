b84aa40634615e17c7ac87e0898d204bfc02caf5
jocnud@gmail.com <jocnud@gmail.com>
2014-05-10 19:53:57 +0530

    Intial Commit

---
+++ maventuts/src/main/java/com/shahbaz/blog/maventuts/App.java
@@ -0,0 +1,12 @@
+package com.shahbaz.blog.maventuts;
+
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println(" Hello world in maven");
+	}
+}
